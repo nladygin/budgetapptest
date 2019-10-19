6a75625f3701ee744206920d5261765bdb1bbadc
nikolay ladygin <nladygin@hrsinternational.com>
2019-10-19 21:58:19 +0500

    profile password tests

--- src/test/java/frontend/ProfileTest.java
+++ src/test/java/frontend/ProfileTest.java
@@ -2,6 +2,7 @@ package frontend;
 
 import frontend.action.LoginAction;
 import frontend.action.ProfileAction;
+import frontend.config.Credentials;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ public class ProfileTest extends BaseTest {
         profileAction.open();
         profileAction.setProfileName(profileName);
         profileAction.checkProfileName(profileName);
+        profileAction.checkSuccessMessage("Successfully update profile");
     }
 
 
@@ -28,11 +30,45 @@ public class ProfileTest extends BaseTest {
         profileAction.open();
         profileAction.setCurrencySign(currencySign);
         profileAction.checkCurrencySign(currencySign);
+        profileAction.checkSuccessMessage("Successfully update profile");
     }
+
+
+    @Test
+    public void changePassword(){
+        loginAction.auth();
+        profileAction.open();
+        profileAction.gotoPassword();
+        profileAction.changeProfilePassword(credentials.password, credentials.password, credentials.password);
+        profileAction.checkSuccessMessage("Successfully change password");
+    }
+
+
+    @Test
+    public void changePasswordWithBadOne(){
+        loginAction.auth();
+        profileAction.open();
+        profileAction.gotoPassword();
+        profileAction.changeProfilePassword(credentials.password, "123", "123");
+        profileAction.checkAlertMessage("Password length must be at least 6 characters");
+    }
+
+
+    @Test
+    public void changePasswordWithBadConfirm(){
+        loginAction.auth();
+        profileAction.open();
+        profileAction.gotoPassword();
+        profileAction.changeProfilePassword(credentials.password, "123456", "123");
+        profileAction.checkAlertMessage("Confirm Password does not match");
+    }
+
+
 
 
 
 
     @Autowired private LoginAction loginAction;
     @Autowired private ProfileAction profileAction;
+    @Autowired private Credentials credentials;
 }
--- src/test/java/frontend/page/ProfilePage.java
+++ src/test/java/frontend/page/ProfilePage.java
@@ -14,9 +14,17 @@ public class ProfilePage extends BasePage{
 
     public final String root = "/profile";
 
-    @FindBy(id = "name")            public WebElement locatorNameInput;
-    @FindBy(id = "currency")        public WebElement locatorCurrencyInput;
-    @FindBy(css = "form button")  public WebElement locatorUpdateButton;
+    @FindBy(id = "name")                                            public WebElement locatorNameInput;
+    @FindBy(id = "currency")                                        public WebElement locatorCurrencyInput;
+    @FindBy(css = "form:nth-child(1) button")                       public WebElement locatorUpdateButton;
+    @FindBy(css = "ul.nav.nav-tabs li:nth-child(2)")                public WebElement locatorPasswordTab;
+    @FindBy(id = "original")                                        public WebElement locatorOriginalPasswordInput;
+    @FindBy(id = "password")                                        public WebElement locatorNewPasswordInput;
+    @FindBy(id = "confirm")                                         public WebElement locatorConfirmPasswordInput;
+    @FindBy(css = "div.alert.alert-success.ng-binding.ng-scope")    public WebElement locatorSuccessMessage;
+    @FindBy(css = "form:nth-child(2) button")                       public WebElement locatorChangePasswordButton;
+    @FindBy(css = "div.form-group.has-feedback.has-error span")   public WebElement locatorAlertMessage;
+
 
 
     @PostConstruct
