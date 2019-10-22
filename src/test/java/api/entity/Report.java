ebd93332f61fed89484dcdf6ac077640ae511091
nikolay ladygin <nladygin@hrsinternational.com>
2019-10-22 10:15:30 +0500

    api cosmetics

--- src/test/java/api/entity/Report.java
+++ src/test/java/api/entity/Report.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
 
 
 @Component
